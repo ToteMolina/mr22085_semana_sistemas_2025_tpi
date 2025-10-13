585a8c54565385ee3715e465e6c90d43663807e3
ToteMolina <totelimo@gmail.com>
2025-10-13 09:38:59 -0600

    Agregando diseño al inicio

--- mvc/app/views/HomeView.php
+++ mvc/app/views/HomeView.php
@@ -1,23 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Semana de Sistemas 2025</title>
 </head>
+
 <body>
     <div class="container">
-        <div class="header">
-            <div class="inicio"><a href="">Inicio</a></div>
-            <div class="dia1"><a href="dia1">Día 1</a></div>
-            <div class="dia2"><a href="dia2">Día 2</a></div>
-            <div class="dia3"><a href="dia3">Día 3</a></div>
-            <div class="dia4"><a href="dia4">Día 4</a></div>
-            <div class="dia5"><a href="dia5">Día 5</a></div>
+        <div class="dia1_inicio">
+            <div class="header">
+                <div class="inicio"><a href="">Inicio</a></div>
+                <div class="dia1"><a href="dia1">Día 1</a></div>
+                <div class="dia2"><a href="dia2">Día 2</a></div>
+                <div class="dia3"><a href="dia3">Día 3</a></div>
+                <div class="dia4"><a href="dia4">Día 4</a></div>
+                <div class="dia5"><a href="dia5">Día 5</a></div>
+            </div>
+            <div class="titulo">
+                <h1>Conectando al futuro</h1>
+            </div>
         </div>
-        <div class="container__descripcion">
-            Conectando al futuro.
+        <div class="info">
+
         </div>
     </div>
 </body>
+
 </html>
