0eaf1314245c01ee0438a30ae66abc9304bc8be9
ToteMolina <totelimo@gmail.com>
2025-10-17 18:07:14 -0600

    Agregando día 5 (sólo el bingo)

---
+++ mvc/app/views/Dia5View.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Día 5 - Semana de Sistemas 2025</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="dia1_sds25">
+            <div class="header">
+                <div class="inicio"><a href="../public/">Inicio</a></div>
+                <div class="dia1"><a href="dia1">Día 1</a></div>
+                <div class="dia2"><a href="dia2">Día 2</a></div>
+                <div class="dia3"><a href="dia3">Día 3</a></div>
+                <div class="dia4"><a href="dia4">Día 4</a></div>
+                <div class="dia5"><a href="dia5">Día 5</a></div>
+                <div class="perfil"><a href="perfil">Perfil</a></div>
+                <div class="registrar"><a href="registrar">Registrar</a></div>
+                <div class="extra"><a href="extra">Extra</a></div>
+            </div>
+
+            <div class="titulo">
+                <h1>Día 5: Bingo + Entrega de diplomas</h1>
+            </div>
+        </div>
+
+        <div class="info">
+            <!-- BINGO -->
+            <div class="ponencia caja">
+                <h2>Bingo</h2>
+                <div class="tema">
+                    <p>Consistía de la siguiente manera: uno se tenía que registrar para poder entrar al sitio web,
+                        una vez adentro se podían comprar créditos, y con esos créditos comprar cartones para poder
+                        jugar.</p>
+                    <p>El administrador creaba la sala y mostraba el código para poder unirse,
+                        uno como usuario sólo tenía que ingresar el código y esperar a que el administrador empezara la
+                        partida.</p>
+                    <p>Una vez ya empezada la partida el administrador escogía el dibujo de la tabla a jugar
+                        e iba presionando un botón en el cual iban saliendo números aleatorios sobre los tablones del
+                        bingo.</p>
+                    <p>Al momento de haber un ganador, éste podía presionar en el botón de <b>"Bingo"</b>
+                        y automáticamente el juego se detenía y mostraba el jugador que había ganado.
+                        Si se daba el caso en el que el jugador presionaba <b>"Bingo"</b> y no tenía todo completado,
+                        era penalizado con <b>30 segundos</b> sin poder hacer nada.</p>
+                </div>
+                <div class="tema caja_foto caja_foto_impar">
+                    <div class="texto caja_foto_texto">
+                        <h3>Indicaciones sobre el bingo</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_1.jpg" alt="Indicaciones sobre el bingo">
+                    </div>
+                </div>
+
+                <div class="tema caja_foto caja_foto_par">
+                    <div class="texto caja_foto_texto">
+                        <h3>Ejemplo de un cartón del bingo</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_2.jpg" alt="Ejemplo de un cartón del bingo">
+                    </div>
+                </div>
+
+                <div class="tema caja_foto caja_foto_impar">
+                    <div class="texto caja_foto_texto">
+                        <h3>Primer ganador</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_3.jpg" alt="Primer ganador">
+                    </div>
+                </div>
+
+                <div class="tema caja_foto caja_foto_par">
+                    <div class="texto caja_foto_texto">
+                        <h3>Segundo ganador</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_4.jpg" alt="Segundo ganador">
+                    </div>
+                </div>
+
+                <div class="tema caja_foto caja_foto_impar">
+                    <div class="texto caja_foto_texto">
+                        <h3>Tercer ganador</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_5.jpg" alt="Tercer ganador">
+                    </div>
+                </div>
+
+                <div class="tema caja_foto caja_foto_par">
+                    <div class="texto caja_foto_texto">
+                        <h3>Cuarto ganador</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_6.jpg" alt="Cuarto ganador">
+                    </div>
+                </div>
+
+                <div class="tema caja_foto caja_foto_impar">
+                    <div class="texto caja_foto_texto">
+                        <h3>Quinto ganador</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_7.jpg" alt="Quinto ganador">
+                    </div>
+                </div>
+
+                <div class="tema caja_foto caja_foto_par">
+                    <div class="texto caja_foto_texto">
+                        <h3>Ambiente en el lugar</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_8.jpg" alt="Ambiente en el lugar">
+                    </div>
+                </div>
+
+                <div class="tema caja_foto caja_foto_impar">
+                    <div class="texto caja_foto_texto">
+                        <h3>Sexto ganador</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_9.jpg" alt="Sexto ganador">
+                    </div>
+                </div>
+
+                <div class="tema caja_foto caja_foto_par">
+                    <div class="texto caja_foto_texto">
+                        <h3>Séptimo ganador</h3>
+                    </div>
+                    <div class="fotografia">
+                        <img src="../public/img/dia5/Bingo/dia5_sds25_bingo_10.jpg" alt="Séptimo ganador">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
